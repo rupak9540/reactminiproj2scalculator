f0092f4700232f1c049349be6726039bf74a6444
Rupak kumar <93849316+rupak9540@users.noreply.github.com>
2022-08-16 14:37:55 +0530

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
 import "./styles.css";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0
+    };
+  }
+  Add = (e) => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number + 2
+      };
+    });
+  };
+  Square = () => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number * prevState.number
+      };
+    });
+  };
+  Double = () => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number * 2
+      };
+    });
+  };
+  Divide = () => {
+    this.setState((prevState) => {
+      return {
+        number: prevState.number / 2
+      };
+    });
+  };
+  Subtract = () => {
+    const { number } = this.state;
+    if (number === 0) {
+      return;
+    }
+    this.setState((prevState) => {
+      return {
+        number: prevState.number - 2
+      };
+    });
+  };
+  Clear = () => {
+    this.setState((prevState) => {
+      return {
+        number: 0
+      };
+    });
+  };
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div className="App">
+        <div className="input">
+          {/* <input type="text"
+       id="fname"
+       placeholder="Enter the value"
+        name="fname"
+        onChange={this.handlechange}
+        value={number}/> */}
+          <div>{number}</div>
+        </div>
+        <div className="button">
+          <div className="add2 space">
+            <button onClick={this.Add}>Add2!</button>
+          </div>
+          <div className="square space">
+            <button onClick={this.Square}>Square!</button>
+          </div>
+          <div className="double space">
+            <button onClick={this.Double}>Double!</button>
+          </div>
+          <div className="divide space">
+            <button onClick={this.Divide}>Divide2!</button>
+          </div>
+          <div className="subtract space">
+            <button onClick={this.Subtract}>Subtract2!</button>
+          </div>
+          <div className="clear space">
+            <button onClick={this.Clear}>Clear</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+export default App;
